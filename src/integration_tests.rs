86c7e3daa6ba94d9a41ab05e43e490552dd969d7
johnoye742 <olugbengajohnoye@gmail.com>
2025-03-29 06:59:07 +0100

    Did some test fixes and added one more test

--- src/integration_tests.rs
+++ src/integration_tests.rs
@@ -53,16 +53,14 @@ mod tests {
             Uint128::new(5)
         );
 
-        let fund_details = FundDetails {
-            owner: Owner {
-                addr: "owner".into_addr(),
-                email: String::from("example@email.com"),
-                fullname: String::from("John Doe")
-            },
+        let msg = InstantiateMsg {
+            email: String::from("example@email.com"),
+            fullname: String::from("John Doe"),
             title: String::from("Example crowdfund"),
-            description: String::from("need some funds")
+            description: String::from("need some funds"),
+            amount_to_be_raised: 500.to_string(),
+            denom: NATIVE_DENOM.into()
         };
-        let msg = InstantiateMsg { details: fund_details };
         let cw_template_contract_addr = app
             .instantiate_contract(
                 cw_template_id,
@@ -80,7 +78,6 @@ mod tests {
     }
 
     mod donations {
-        use cosmwasm_std::Response;
 
         use super::*;
         use crate::{msg::{ExecuteMsg, QueryMsg}, state::Donation};
@@ -90,7 +87,7 @@ mod tests {
             let (mut app, cw_template_contract) = proper_instantiate();
 
             let msg = ExecuteMsg::Donate { message: "Enjoy!".into() };
-            let resp = app.execute_contract(USER.into_addr(), cw_template_contract.addr(), &msg, &coins(3, "eth"));
+            let resp = app.execute_contract(USER.into_addr(), cw_template_contract.addr(), &msg, &coins(3, NATIVE_DENOM));
 
             println!("response: {:?}", resp);
 
@@ -125,5 +122,24 @@ mod tests {
             println!("ADMIN balance after: {:?}", app.wrap().query_balance(ADMIN.into_addr(), NATIVE_DENOM));
             println!("USER balance after: {:?}", app.wrap().query_balance(USER.into_addr(), NATIVE_DENOM));
         }
+
+        #[test]
+        fn query_details() {
+            let (app, cw_template_contract) = proper_instantiate();
+            let details: FundDetails = app.wrap().query_wasm_smart(cw_template_contract.addr(), &QueryMsg::GetDetails {  }).unwrap();
+
+            assert_eq!(details, FundDetails {
+                owner: Owner {
+                    email: String::from("example@email.com"),
+                    fullname: String::from("John Doe"),
+                    addr: ADMIN.into_addr()
+                },
+                title: String::from("Example crowdfund"),
+                description: String::from("need some funds"),
+                amount_to_be_raised: Uint128::new(500),
+                denom: NATIVE_DENOM.into()
+            });
+        }
+
     }
 }
