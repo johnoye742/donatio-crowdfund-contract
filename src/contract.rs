8e6f8aef622a2e5bc687e43880fae4956304a48e
johnoye742 <olugbengajohnoye@gmail.com>
2025-04-04 01:54:17 +0100

    Removed unnecessary stuff

--- src/contract.rs
+++ src/contract.rs
@@ -1,11 +1,11 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{coins, to_json_binary, BankMsg, Binary, Decimal, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint128};
+use cosmwasm_std::{to_json_binary, BankMsg, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint128};
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
-use crate::msg::{ExecuteMsg, GetCountResponse, InstantiateMsg, QueryMsg};
-use crate::state::{Donation, FundDetails, Owner, State, DETAILS, DONATIONS, STATE};
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{Donation, FundDetails, Owner, DETAILS, DONATIONS};
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:crowdfund-contract";
@@ -83,7 +83,6 @@ pub fn execute(
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        QueryMsg::GetCount {} => to_json_binary(&query::count(deps)?),
         QueryMsg::GetDonations {  } => {
             to_json_binary(&DONATIONS.load(deps.storage).unwrap_or(vec![]))
         },
@@ -93,42 +92,3 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     }
 }
 
-pub mod query {
-    use super::*;
-
-    pub fn count(deps: Deps) -> StdResult<GetCountResponse> {
-        let state = STATE.load(deps.storage)?;
-        Ok(GetCountResponse { count: state.count })
-    }
-}
-
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cosmwasm_std::coins;
-
-    #[test]
-    fn proper_initialization() {
-        let mut deps = mock_dependencies();
-        let msg = InstantiateMsg {
-            email: String::from("example@email.com"),
-            fullname: String::from("John Doe"),
-            title: String::from("Example crowdfund"),
-            description: String::from("need some funds"),
-            amount_to_be_raised: 500.to_string(),
-            denom: "usdc".into(),
-            image_url: String::new()
-        };
-
-        let info = mock_info("creator", &coins(1000, "earth"));
-
-        // we can just call .unwrap() to assert this was a success
-        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-        assert_eq!(0, res.messages.len());
-
-    }
-
-
-}
