d38acb597119b30a82ded0aee655cec2b4001933
johnoye742 <olugbengajohnoye@gmail.com>
2025-04-07 23:04:14 +0100

    Added new errors to be thrown depending on contract state

--- src/error.rs
+++ src/error.rs
@@ -8,6 +8,15 @@ pub enum ContractError {
 
     #[error("Unauthorized")]
     Unauthorized {},
+
+    #[error("Fundraiser Closed")]
+    FundraiserCloseed {},
+
+    #[error("Fundraiser Pending, please wait.")]
+    FundraiserPending {},
+
+    #[error("This fundraiser has being cancelled")]
+    FundraiserCanceled {},
     // Add any other custom errors you like here.
     // Look at https://docs.rs/thiserror/1.0.21/thiserror/ for details.
 }
