f6b1aa3e9b4d903b052a3b27d81b5f10100c4271
johnoye742 <olugbengajohnoye@gmail.com>
2025-04-28 10:52:09 +0100

    Update integration_tests.rs

--- src/integration_tests.rs
+++ src/integration_tests.rs
@@ -34,18 +34,10 @@ mod tests {
                 .init_balance(
                     storage,
                     &MockApi::default().addr_make(USER),
-                    coins(50, NATIVE_DENOM),
+                    coins(600*1000000, NATIVE_DENOM),
                 )
                 .unwrap();
 
-            router
-                .bank
-                .init_balance(
-                    storage,
-                    &MockApi::default().addr_make(USER),
-                    coins(5, NATIVE_DENOM),
-                )
-                .unwrap();
         })
     }
 
@@ -53,18 +45,12 @@ mod tests {
         let mut app = mock_app();
         let cw_template_id = app.store_code(contract_template());
 
-        let user = app.api().addr_make(USER);
-        assert_eq!(
-            app.wrap().query_balance(user, NATIVE_DENOM).unwrap().amount,
-            Uint128::new(5)
-        );
-
         let msg = InstantiateMsg {
             email: String::from("example@email.com"),
             fullname: String::from("John Doe"),
             title: String::from("Example crowdfund"),
             description: String::from("need some funds"),
-            amount_to_be_raised: 500.to_string(),
+            amount_to_be_raised: (5*1000000).to_string(),
             denom: NATIVE_DENOM.into(),
             image_url: String::new(),
             owner: USER.into_addr()
@@ -87,6 +73,8 @@ mod tests {
 
     mod donations {
 
+        use std::u128;
+
         use super::*;
         use crate::msg::{ExecuteMsg, QueryMsg};
 
@@ -95,11 +83,10 @@ mod tests {
             let (mut app, cw_template_contract) = proper_instantiate();
 
             let msg = ExecuteMsg::Donate { message: "Enjoy!".into() };
-            let resp = app.execute_contract(USER.into_addr(), cw_template_contract.addr(), &msg, &coins(5, NATIVE_DENOM));
-            app.execute_contract(USER.into_addr(), cw_template_contract.addr(), &msg, &coins(5*1000000, NATIVE_DENOM));
-            app.execute_contract(USER.into_addr(), cw_template_contract.addr(), &msg, &coins(5*1000000, NATIVE_DENOM));
-            println!("response: {:?}", resp);
-
+            let x: u128 = 5*1000000;
+            let resp = app.execute_contract(USER.into_addr(), cw_template_contract.addr(), &msg, &[Coin { amount: Uint128::from(x), denom: NATIVE_DENOM.into() }, ]);
+            println!("donate response: {:?}", resp);
+            assert!(resp.is_ok());
             query_total(&app, cw_template_contract.clone());
 
             withdraw(app, cw_template_contract);
@@ -107,17 +94,11 @@ mod tests {
         }
 
         fn withdraw(mut app: App, cw_template_contract: CwTemplateContract) {
-            println!("ADMIN balance before: {:?}", app.wrap().query_balance(ADMIN.into_addr(), NATIVE_DENOM));
-            println!("USER balance before: {:?}", app.wrap().query_balance(USER.into_addr(), NATIVE_DENOM));
-
-            println!("Contract balance: {:?}", &app.wrap().query_balance(cw_template_contract.addr(), "eth"));
 
 
             let resp = app.execute_contract(USER.into_addr(), cw_template_contract.addr(), &ExecuteMsg::Withdraw {}, &[]);
-
-            println!("Withdraw response: {:?}", resp);
-            println!("ADMIN balance after: {:?}", app.wrap().query_balance(ADMIN.into_addr(), NATIVE_DENOM));
-            println!("USER balance after: {:?}", app.wrap().query_balance(USER.into_addr(), NATIVE_DENOM));
+            println!("withdraw response: {:?}", resp);
+            assert!(resp.is_ok());
         }
 
         #[test]
@@ -133,7 +114,7 @@ mod tests {
                 },
                 title: String::from("Example crowdfund"),
                 description: String::from("need some funds"),
-                amount_to_be_raised: Uint128::new(500),
+                amount_to_be_raised: Uint128::new(5*1000000),
                 denom: NATIVE_DENOM.into(),
                 image_url: String::new()
             });
